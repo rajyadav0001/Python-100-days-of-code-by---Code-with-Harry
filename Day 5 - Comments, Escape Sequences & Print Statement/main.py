dcd930268d1cb5b0bd85e221106827e4ff1344e8
rajyadav0001 <rajneshsbl9536@gmail.com>
2022-12-06 22:41:39 +0530

    Day 4 & 5 done

---
+++ Day 5 - Comments, Escape Sequences & Print Statement/main.py
@@ -0,0 +1,28 @@
+
+
+
+# This is how we can keep   "Single line"   comments in Python
+print("Hey i am a good boy \nand this viewer is also a good boy")
+
+'''
+   --->>>>> using "alt+up/down arrow" we can move code up and down
+   --->>>>> using "alt+left/right arrow" we can move from one window
+           to another window which is opende on top of VS code
+
+
+      this is how we can use multi line comments in Python
+  '''
+
+#   This is Escape Sequences Character
+# when we need to use string whitin a string this is how we can use 
+print("Hey i am a  \"good boy\" \nand this viewer is also a good boy")
+
+print("Hey",6,7,sep="\n")
+'''
+seprater is used to print anthing between first give objects 
+like print("Hey",6,7,sep=" - ")
+for this output will be-->   Hey - 6 - 7
+'''
+
+print("Hey",6,7,sep="\n",end=" 0001\n")
+# End statemet spacify what to print in the end of the code output
