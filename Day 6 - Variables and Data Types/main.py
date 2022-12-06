f1e3f6a2ef6dac3f0893b740a5799ea91b535847
rajyadav0001 <rajneshsbl9536@gmail.com>
2022-12-06 23:26:04 +0530

    Day 6 done

---
+++ Day 6 - Variables and Data Types/main.py
@@ -0,0 +1,42 @@
+
+# EVERYTHING IS OBJECT IN PYTHON
+
+# THERE IS 3 TYPE OF NUMARIC DATA TYPE
+
+# 1.integer data type
+a = 5,-8,0
+print("Output of a is -->",a)
+
+# 2.Float data type
+a1 = 4.4,-8.6,0.00001
+print("Output of a1 is -->",a1)
+
+# 3.Complex data type
+a2 = complex(5+5)
+print("Output of a2 is -->",a2)
+
+# TEXT DATA TYPE 
+# 1.String data type
+b = "Raj Yadav"
+print(b)
+
+# BOOLEAN DATA TYPE
+# 1.boolean data type
+c = True
+print(c)
+
+# SEQUENCED DATA TYPE
+# 1.list --> will be explaned in upcoming lectures
+# 2.tuple --> will be explaned in upcoming lectures
+
+# MAP DATA TYPE
+# 1. Dictionary --> will be explaned in upcoming lectures
+# none data type
+d = None
+print(d)
+
+a1 = 6
+
+# To know what is the data type
+print("The type of b is", type(b))
+
