f7f4785301aa6740a1635b084a0e1961938f8ab8
rajyadav0001 <rajneshsbl9536@gmail.com>
2022-12-07 00:12:20 +0530

    Day 7 done

---
+++ Day 7 Mathmatical opraters/Simple Calculator.py
@@ -0,0 +1,22 @@
+
+a = 15
+b = 4
+
+print("\n")
+ans1 = a+b
+print("Addition of",a, "and",b, "is --> ", ans1,"\n")
+
+ans2 = a-b
+print("Subtraction of",a, "and",b, "is --> ", ans2,"\n")
+
+ans3 = a*b
+print("Multiplacation of",a, "and",b, "is --> ", ans3,"\n")
+
+ans4 = a/b
+print("Division of",a, "and",b, "is --> ", ans4,"\n")
+
+ans5 = a//b
+print("Floor Division of",a,"and",b,"is -->", ans5,"\n")
+
+ans6 = a%b
+print("Modulo of",a,"and",b,"is -->", ans6,"\n")
---
+++ Day 7 Mathmatical opraters/main.py
@@ -0,0 +1,26 @@
+
+# Python has different types of operators for different operations.
+# Which is mention below
+
+# Addition
+print("Addition is -->", 5+6)
+
+# Subtraction
+print("Subtraction is -->",15-6)
+ 
+# Multiplication	
+print("Multiplication is -->",15*6)
+
+# Exponential
+print("Exponential is -->",2**4)
+
+# Division
+print("Division is -->",15/6)
+
+# Floor Division	
+print("Floor Division is -->",15//6)
+
+# Modulus
+print("Modulus is -->",5%3)
+
+ 
