9c7f196f8b3a8239269092790d7ea3c9f115df73
rajyadav0001 <rajneshsbl9536@gmail.com>
2022-12-08 17:10:42 +0530

    Taking user input in py

---
+++ Day 10 - Taking User Input in Python/User input.py
@@ -0,0 +1,10 @@
+
+
+# python input function always take input as a string
+a = input("Please enter your name : ")
+print("My name is",a)
+
+x = input("Enter first no : ")
+y = input("Enter second no : ")
+print(x + y)  #it will return no by concanation and adding
+print(int(x) + int(y))
