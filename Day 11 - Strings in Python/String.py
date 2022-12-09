9f3e1c9cb92ed7c8de387ad2240e1e3f2cb0f7ba
rajyadav0001 <rajneshsbl9536@gmail.com>
2022-12-09 19:43:49 +0530

    Day 11 done

---
+++ Day 11 - Strings in Python/String.py
@@ -0,0 +1,29 @@
+name = "Rajnesh"
+friend = "Raj"
+anotherfriend = 'Yadav'
+
+print("Hello ," +name)
+
+# multiline string needs to be coted in '''  cots
+mulstringn = ''' 
+      Hi, my name is Rajnesh Yadav
+       i'm form Uttar Pradesh,india
+'''
+print(mulstringn)
+
+
+# printing characters using indexing
+print("By using indexing\n")
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+
+print("By using for loop\n")
+for character in name:
+     print(character)
+
+
