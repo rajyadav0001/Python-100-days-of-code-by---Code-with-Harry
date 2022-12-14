4ce28706ef64535ebe28d40adc852ff531c49389
rajyadav0001 <rajneshsbl9536@gmail.com>
2022-12-14 13:09:29 +0530

    Day 12 done

---
+++ Day 12 - Strings Slicing and Operations on Strings in Python/stringslicing.py
@@ -0,0 +1,22 @@
+
+
+names = "Rajnseh"
+print(len(names))
+
+print(names[0:5])
+
+# IF WE WILL NOT MENTON STARTING POINT IT WILL START FORM 0 INDEX
+print(names[ :5])
+
+# IF WE WILL NOT MENTON ENDING POINT IT WILL GO TILL END INDEX
+print(names[ 0:])
+
+# NEGATIVE SLICING WILL REDUCE GIVEN NUMBER FROM TOTOAL LENGTH 
+print(names[ 0:-3])  #0:7-3=> 0:4
+
+print(names[ -3:-1])  #7-3:7-1=> 4:6 =>sh
+
+
+nm = "Harry"
+print(nm[-4:-2])
+
